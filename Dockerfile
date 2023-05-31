33f7ff5f976d1d3f382d7db16432b6d8712bc533
Steve_Kim <stevekim724@gmail.com>
2023-05-31 10:32:23 +0000

    first

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM educative1/ff1
+
+RUN wget --quiet -O sdk.install.sh "https://get.sdkman.io" && \
+    bash -c "bash ./sdk.install.sh > /dev/null && source ~/.sdkman/bin/sdkman-init.sh && sdk install kotlin && sdk install java 11.0.2-open && sdk install gradle 7.5 && export JAVA_HOME=/root/.sdkman/candidates/java/current/ && cd /tmp/sample/android && gradle wrapper --build-cache && cd .. && flutter pub get && cd android && gradle assembleDebug --build-cache && cd .. && flutter build apk --debug" && \
+    rm -f sdk.install.sh
--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,55 @@
+import 'package:educative/Views/Splash.dart';
+import 'package:educative/Views/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:educative/Views/Login.dart';
+import 'package:educative/Views/Profile.dart';
+import 'package:educative/Views/SignUp.dart';
+import 'package:educative/controllers/AuthController.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await GoogleSignIn().signInSilently();
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAmOB_-8DIEIwJEOvrM2a8C6RE-KbvSDDc",
+          projectId: "educative-9",
+          storageBucket: "educative-9.appspot.com",
+          messagingSenderId: "14205101883",
+          appId: "1:14205101883:web:7c0db5d88bc2dc0310791e"),
+    ).then((value) => Get.put(GetAuth()));
+    runApp(const MyApp());
+  } else {
+    await GoogleSignIn().signInSilently();
+    await Firebase.initializeApp().then((value) => Get.put(GetAuth()));
+    runApp(const MyApp());
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
+    return GetMaterialApp(
+      title: 'Tree Life',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(),
+      getPages: [
+        GetPage(name: '/', page: () => const SplashScreen()),
+        GetPage(name: '/login', page: () => const LoginInPage()),
+        GetPage(name: '/signup', page: () => const SignUpPage()),
+        GetPage(name: '/Profile', page: () => UserProfileScreen()),
+        GetPage(name: '/Welcome', page: () => WelcomePage()),
+      ],
+      initialRoute: '/',
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('Flutter Demo Click Counter'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 25),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
